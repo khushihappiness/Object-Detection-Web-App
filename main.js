871f658893fb7a6e362a266fe75599683afb34f4
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-11-15 15:24:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 img="";
 status="";
+objects =[];
+
 function preload()
 {
     img=loadImage('dog_cat.jpg');
@@ -31,20 +33,39 @@ console.error(error);
     else
     {
         console.log(results);
+        objects =results;
     }
 }
 function draw()
 {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+//Manually to draw the rectangle
 
-    fill("#FF0000");
-    text("cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320)
+   // fill("#FF0000");
+    //text("Dog", 45, 75);
+    //noFill();
+    //stroke("#FF0000");
+    //rect(30, 60, 450, 350);
+
+    //fill("#FF0000");
+    //text("cat", 320, 120);
+    //noFill();
+    //stroke("#FF0000");
+    //rect(300, 90, 270, 320)
+
+    //Drawing the rectangle with the help of cocossd 
+
+    if(status != "")
+    {
+        for(i= 0; i< objects.length; i++)
+        {
+            document.getElementById("status").innerHTML= "Status : Object Detected";
+         fill("#FF0000");
+         percent= floor(objects[i].confidence*100);
+         text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y)
+         noFill();
+         stroke("#FF0000");
+         rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
